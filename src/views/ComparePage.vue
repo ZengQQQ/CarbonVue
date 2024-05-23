a3d82d59a87ba4de0d45b459c841c911e3bec916
Zcqer <2200304515@qq.com>
2024-05-23 16:03:27 +0800

    graphData, showData.vue

--- src/views/ComparePage.vue
+++ src/views/ComparePage.vue
@@ -1,8 +1,17 @@
 <script setup>
+import GraphData from "@/components/ComparePage/graphData.vue";
+import ShowData from "@/components/ComparePage/ShowData.vue";
 </script>
 
 
 <template>
+    <div class ="dataMap">
+        <GraphData />
+    </div>
+
+    <div class="dataShow">
+        <ShowData />
+    </div>
 
 </template>
 
