172c45f80e5198bb5e169f0b2c40c327c3473e56
Zcqer <2200304515@qq.com>
2024-05-23 16:11:39 +0800

    spilt home

--- src/views/ComparePage.vue
+++ src/views/ComparePage.vue
@@ -1,18 +1,9 @@
 <script setup>
-import GraphData from "@/components/ComparePage/graphData.vue";
-import ShowData from "@/components/ComparePage/ShowData.vue";
+
 </script>
 
 
 <template>
-    <div class ="dataMap">
-        <GraphData />
-    </div>
-
-    <div class="dataShow">
-        <ShowData />
-    </div>
-
 </template>
 
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,20 @@
 <script setup>
+import GraphData from "@/components/Home/GraphData.vue";
+import ShowData from "@/components/Home/ShowData.vue";
 </script>
 
 
 <template>
-    
+    <div class ="dataMap">
+        <GraphData />
+    </div>
+
+    <div class="dataShow">
+        <ShowData />
+    </div>
+
 </template>
+
+
+<style scoped>
+</style>
